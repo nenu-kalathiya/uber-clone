756246559df4e1b926009894bb108db8eede5e74
Nency-kalathiya <nency.kalathiya@swiftut.com>
2024-12-30 21:23:49 +0530

    First commit

---
+++ Backend/services/user.service.js
@@ -0,0 +1,9 @@
+const userModel = require('../models/user.model');
+
+module.exports.createUser = async ({
+    firstname, lastname, email, password
+}) => {
+    if(!firstname || !lastname || !email || !password) {
+        throw new Error('All fields are required');
+    }
+}
